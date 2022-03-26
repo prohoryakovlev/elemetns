77b77125e943607d53e13edeb58b5ed52af2fa59
Prohor <prohor-yakovlev@mail.ru>
2022-03-26 14:06:04 +0300

    actions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const box = document.getElementById('box'),
-      btns = document.querySelector('button'),
+      btns = document.getElementsByTagName('button'),
       circles = document.getElementsByClassName('circle'),
       hearts = document.querySelectorAll('heart'),
       oneHeart = document.querySelector('.heart');
+
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = `background-color: blue; width: 500px`;
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
 
