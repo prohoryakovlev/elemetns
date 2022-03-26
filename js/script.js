48f21cf8232dc84c3a1bc2d79d76b0ffba502295
Prohor <prohor-yakovlev@mail.ru>
2022-03-26 15:02:49 +0300

    methods

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@
 const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('heart'),
-      oneHeart = document.querySelector('.heart');
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
 
 
 // box.style.backgroundColor = 'blue';
@@ -15,4 +16,28 @@ box.style.cssText = `background-color: blue; width: 500px`;
 btns[1].style.borderRadius = '100%';
 circles[0].style.backgroundColor = 'red';
 
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+  item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был Я');
+
+div.classList.add('black');
+
+// wrapper.append(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+
+// hearts[0].after(div);
+
+// circles[0].remove();
+
+// hearts[0].replaceWith(circles[0]);
 
